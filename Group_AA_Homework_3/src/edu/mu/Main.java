fd42a7c86ec42968b6a3bc545877d020fe9cbf8f
Connor Joyce <steelcity2003@gmail.com>
2024-02-13 12:36:40 -0600

    tested add, remove an update price methods in main

--- Group_AA_Homework_3/src/edu/mu/Main.java
+++ Group_AA_Homework_3/src/edu/mu/Main.java
@@ -27,10 +27,33 @@ public class Main {
 		
 		stockManager.print(tapeList);
 		
+		
+		
+		System.out.println("Change Item Price:\n");
+		
+		MediaProduct product = stockManager.inventory.get(0);
+		
+		stockManager.updateItemPrice(product, 15.99);
+		
 		System.out.println("Reprint Inventory:\n");
 		
 		stockManager.print(stockManager.inventory);
 		
+		stockManager.removeItem(stockManager.inventory.get(1));
+		
+		System.out.println("Reprint Inventory:\n");
+		
+		stockManager.print(stockManager.inventory);
+		
+		Genre genre = Genre.valueOf("HIP_HOP");
+		
+		CDRecordProduct newProduct = new CDRecordProduct("New Item", 9.99, 2009, genre);
+		
+		stockManager.addItem(newProduct);
+		
+		System.out.println("Reprint Inventory:\n");
+		
+		stockManager.print(stockManager.inventory);
 
 	}
 
