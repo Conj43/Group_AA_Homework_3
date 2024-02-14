cd2313a047f39e10c51e0b2f62d7efa96c8cb903
Connor Joyce <steelcity2003@gmail.com>
2024-02-13 18:49:15 -0600

    created extra tests and added onto some methods
    we test each method in main class and print out after each test

--- Group_AA_Homework_3/src/edu/mu/Main.java
+++ Group_AA_Homework_3/src/edu/mu/Main.java
@@ -5,55 +5,95 @@ import java.util.ArrayList;
 import edu.mu.mediaproducts.*;
 
 public class Main {
+	private static boolean test = false;
+	
+	public static void print() {
+		
+		System.out.println("\nTest: " + test + "\n");
+	}
 
 	
 
 	public static void main(String[] args) {
+		
+		
 		StockManagerSingleton stockManager = new StockManagerSingleton();
 		
-		stockManager.initalizeStock();
+		test = stockManager.initalizeStock();
 		
-		stockManager.print(stockManager.inventory);
+		System.out.println("First Print:\n");
 		
-		System.out.println("CDs:\n");
+		print();
+		
+		stockManager.printListOfMediaProduct(stockManager.inventory);
+		
+		System.out.println("\nCDs:\n");
 		
 		ArrayList<CDRecordProduct> cdList = stockManager.getCDRecordsList(stockManager.inventory);
 		
 		stockManager.print(cdList);
 		
-		System.out.println("Tapes:\n");
+		System.out.println("\nTapes:\n");
 		
 		ArrayList<TapeRecordProduct> tapeList = stockManager.getTapeRecordsList(stockManager.inventory);
 		
 		stockManager.print(tapeList);
 		
+		System.out.println("\nVinyls:\n");
 		
+		ArrayList<VinylRecordProduct> vinylList = stockManager.getVinylRecordList(stockManager.inventory);
 		
-		System.out.println("Change Item Price:\n");
+		stockManager.print(vinylList);
+		
+		System.out.println("\nChanging first Item's price...\n");
 		
 		MediaProduct product = stockManager.inventory.get(0);
 		
-		stockManager.updateItemPrice(product, 15.99);
+		test = stockManager.updateItemPrice(product, 20.50);
+		
+		print();
 		
 		System.out.println("Reprint Inventory:\n");
 		
 		stockManager.print(stockManager.inventory);
 		
-		stockManager.removeItem(stockManager.inventory.get(1));
+		test = stockManager.removeItem(stockManager.inventory.get(1));
+		
+		System.out.println("\nRemoving the second Item...\n");
+		
+		print();
 		
 		System.out.println("Reprint Inventory:\n");
 		
 		stockManager.print(stockManager.inventory);
+		
+		System.out.println("\nAdding a new Product at the end of the list...\n");
 		
 		Genre genre = Genre.valueOf("HIP_HOP");
 		
 		CDRecordProduct newProduct = new CDRecordProduct("New Item", 9.99, 2009, genre);
 		
-		stockManager.addItem(newProduct);
+		test = stockManager.addItem(newProduct);
+		
+		print();
 		
 		System.out.println("Reprint Inventory:\n");
 		
 		stockManager.print(stockManager.inventory);
+		
+		ArrayList<MediaProduct> belowPriceList = new ArrayList<>();
+		
+		belowPriceList = stockManager.getMediaProductBelowPrice(20);
+		
+		System.out.println("\nMedia Products Under $20:\n");
+		
+		stockManager.print(belowPriceList);
+		
+		
+		
+		test = stockManager.saveStock();
+		
+		print();
 
 	}
 
